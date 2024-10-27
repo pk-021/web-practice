48b35c64a6ccdcca7144e9150758af8a25bc58d0
Pawan Kharel <pawankharel10@gmail.com>
2024-10-27 09:54:15 +0545

    scrollAnimation

---
+++ scrollAnimation/app.js
@@ -0,0 +1,27 @@
+const cards = document.querySelectorAll(".card");
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("show", entry.isIntersecting)
+        // if (entry.isIntersecting)
+        // {
+        //     observer.unobserve(entry.target);
+        // }
+    }
+    )
+}
+,
+{
+    threshold:0.4
+}
+)
+
+cards.forEach(card => 
+{
+    observer.observe(card);
+}
+)
+
+
+
+
